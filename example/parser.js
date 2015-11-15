7689442387ee4e0fd7fcafafaaa5e74ec9c80d2b
Paul Carduner <paul@carduner.net>
2015-11-15 12:35:22 -0800

    add peekToken method to example parser

--- example/parser.js
+++ example/parser.js
@@ -32,7 +32,7 @@ export default class Parser {
     return ch
   }
 
-  getToken(depth = 0) {
+  getToken() {
     const IDENTIFIER_RE = /[\w-+\/*]/
     if (this.charIndex >= this.code.length) {
       return new Token(
@@ -64,7 +64,7 @@ export default class Parser {
       while (this.code[this.charIndex] == ' ') {
         this.getch()
       }
-      return this.getToken(depth+1)
+      return this.getToken()
     } else if (this.code[this.charIndex] >= '0' && this.code[this.charIndex] <= '9') {
       let startIndex = this.colIndex
       let number = ''
@@ -91,10 +91,21 @@ export default class Parser {
       )
     } else if (this.code[this.charIndex] == '\n') {
       this.getch()
-      return this.getToken(depth+1)
+      return this.getToken()
     } else {
       throw new Error("parse error")
     }
+  }
+
+  peekToken() {
+    let oldCharIndex = this.charIndex
+    let oldLineIndex = this.lineIndex
+    let oldColIndex = this.colIndex
+    let token = this.getToken()
+    this.charIndex = oldCharIndex
+    this.lineIndex = oldLineIndex
+    this.colIndex = oldColIndex
+    return token
   }
 
   parse(code) {
@@ -107,37 +118,35 @@ export default class Parser {
   }
 
   parseExpression() {
-    var token = this.getToken()
+    let token = this.getToken()
     if (token.token != TOKENS.OPEN_PAREN) {
       throw new Error("Expected an open paren")
     }
-    var identifierToken = this.getToken()
-    if (identifierToken.token != TOKENS.IDENTIFIER) {
+    if (this.peekToken().token != TOKENS.IDENTIFIER) {
       throw new Error("Expected an identifier")
     }
+    let identifierToken = this.getToken()
     var args = []
-    var currentToken = this.getToken()
-    while (currentToken.token != TOKENS.CLOSE_PAREN) {
-      switch (currentToken.token) {
+    while (this.peekToken().token != TOKENS.CLOSE_PAREN) {
+      switch (this.peekToken().token) {
       case TOKENS.OPEN_PAREN:
-        this.charIndex--
-        this.colIndex--
         args.push(this.parseExpression())
         break
       case TOKENS.NUMBER:
+        var literalToken = this.getToken()
         args.push(
           new Literal(
-            currentToken.from,
-            currentToken.to,
-            parseInt(currentToken.text)
+            literalToken.from,
+            literalToken.to,
+            parseInt(literalToken.text)
           )
         )
         break
       default:
         throw new Error("Expected either a number or another expression")
       }
-      currentToken = this.getToken()
     }
-    return new Expression(token.from, currentToken.to, identifierToken.text, args)
+    let closeParenToken = this.getToken()
+    return new Expression(token.from, closeParenToken.to, identifierToken.text, args)
   }
 }
