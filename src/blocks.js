89c10af7abf7a064eb00e51227f708b62f540b20
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2015-12-14 17:43:58 -0500

    factor error message retrieval out into the parser class

--- src/blocks.js
+++ src/blocks.js
@@ -138,10 +138,7 @@ export default class CodeMirrorBlocks {
       return true;
     } catch (e) {
       nodeEl.classList.add('blocks-error');
-      let msg = eval('('+e+')')['dom-message'][2].slice(2);
-      // Use an error message, if one is available. Otherwise give a generic hint
-      nodeEl.title = "Error:" + (msg.every((element) => typeof element==="string") ?
-                                 msg : "Check your quotation marks, or any other symbols you've used");
+      nodeEl.title = this.parser.getExceptionMessage(e);
       console.error("result doesn't parse", e);
       return false;
     }
