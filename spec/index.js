449f7a75601db4b7dc64c45c3d7d77c1fd14ce37
Paul Carduner <paul@carduner.net>
2015-12-18 17:44:40 -0800

    include all subdirectories when tests run.

--- spec/index.js
+++ spec/index.js
@@ -2,8 +2,5 @@
 require('babel-polyfill');
 
 // require all the files in the spec folder that end with -test.js
-var context = require.context('.', false, /.*-test.js$/);
-context.keys().forEach(context);
-// require all the files in the parsers folder
-context = require.context('./parsers', false, /.*-test.js$/);
+var context = require.context('.', true, /.*-test.js$/);
 context.keys().forEach(context);
