827efc71bd8b4c21331d5ee60544215acd74b67f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2016-01-07 06:38:17 -0500

    fix tree walk so that expressions in the function position can be added to the nextMap and previousMap

--- src/ast.js
+++ src/ast.js
@@ -101,7 +101,7 @@ export class Expression extends ASTNode {
   *[Symbol.iterator]() {
     yield this;
     if (this.func instanceof ASTNode) {
-      yield this.func;
+      yield* this.func;
     }
     for (let arg of this.args) {
       for (let node of arg) {
