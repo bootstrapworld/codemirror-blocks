3efb66b5d785175033815ad13cd2456c50ed7a7a
Paul Carduner <paul@carduner.net>
2016-01-23 18:06:00 -0800

    speed up example app rebuilds with improved webpack config

--- webpack/example.config.js
+++ webpack/example.config.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var path = require('path');
+var webpack = webpack = require('webpack');
 var baseConfig = require('./base.config.js');
 
 // this is the config for generating the files needed to run the examples.
@@ -6,7 +8,8 @@ module.exports = _.extend({}, baseConfig, {
   devtool: 'eval',
   entry: {
     "example": './example/example.js',
-    "wescheme-example": './example/wescheme-example.js'
+    "wescheme-example": './example/wescheme-example.js',
+    "third-party": ['react', 'react-dom', 'babel-polyfill', 'codemirror']
   },
   module: _.extend({}, baseConfig.module, {
     loaders: baseConfig.module.loaders.concat([
@@ -14,5 +17,18 @@ module.exports = _.extend({}, baseConfig, {
       { test: /\.css$/, loaders: ["style", "css"] },
       { test: /\.rkt$/, loader: 'raw' }
     ])
-  })
+  }),
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name:'third-party',
+      minChunks: Infinity
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    hot: true,
+    inline: true,
+    progress: true,
+    contentBase: path.join(__dirname, '..', 'example')
+  }
 });
