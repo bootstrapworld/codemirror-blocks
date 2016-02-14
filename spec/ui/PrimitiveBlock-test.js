1e8456ccc342e50ec5b2365777bed53f7ef6833c
Paul Carduner <paul@carduner.net>
2016-02-13 17:47:52 -0800

    add more tests for PrimitiveBlock component

--- spec/ui/PrimitiveBlock-test.js
+++ spec/ui/PrimitiveBlock-test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
+import {Literal} from '../../src/ast';
 import {dragstart} from '../events';
 import {Primitive} from '../../src/parsers/primitives';
 import PrimitiveBlock from '../../src/ui/PrimitiveBlock';
@@ -23,4 +24,28 @@ describe('The PrimitiveBlock component,', function() {
     el.firstChild.dispatchEvent(dragEvent);
     expect(dragEvent.dataTransfer.getData('text/plain')).toBe('some-primitive');
   });
+
+  it('should render a draggable node based on the ast node provided by the primitive', function() {
+    let primitive = new Primitive({}, 'some-primitive');
+    primitive.getASTNode = () => new Literal(
+      {line: 0, ch: 0},
+      {line: 0, ch: 0},
+      'some-primitive',
+      'symbol'
+    );
+    let primitiveBlock = TestUtils.renderIntoDocument(<PrimitiveBlock primitive={primitive}/>);
+    let renderedBlockNode = TestUtils.findRenderedComponentWithType(
+      primitiveBlock, RenderedBlockNode);
+    let el = ReactDOM.findDOMNode(renderedBlockNode.refs.root);
+
+    let dragEvent = dragstart();
+    el.firstChild.dispatchEvent(dragEvent);
+    expect(dragEvent.dataTransfer.getData('text/plain')).toBe('some-primitive');
+
+  });
+
+  it('should render an empty div if no primitive is provided', function() {
+    TestUtils.renderIntoDocument(<PrimitiveBlock primitive={null}/>);
+  });
+
 });
