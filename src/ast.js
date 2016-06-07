ae1ddd9a25623cafc826eed5865035c6e7fc4a8d
Benjamin Limpich <benlimpich@gmail.com>
2016-06-07 16:54:49 -0700

    added assignment, binary expression, sequences (prog) and Blocked Scope Variables (let) to ast node types

--- src/ast.js
+++ src/ast.js
@@ -183,17 +183,16 @@ export class FunctionDefinition extends ASTNode {
 
 //TODO: make this work
 export class Conditional extends ASTNode {
-  constructor(from, to, ifStatement, thenStatement, elseStatement, options={}) {
-    super(from, to, 'conditional', options);
-    this.ifStatement = ifStatement;
+  constructor(from, to, condStatement, thenStatement, elseStatement, options={}) {
+    super(from, to, 'condStatementitional', options);
+    this.condStatement = condStatement;
     this.thenStatement = thenStatement;
     this.elseStatement = elseStatement;
   }
 
   *[Symbol.iterator]() {
     yield this;
-    yield this.name;
-    for (let node of this.ifStatement) {
+    for (let node of this.condStatement) {
       yield node;
     }
     for (let node of this.thenStatement) {
@@ -208,9 +207,84 @@ export class Conditional extends ASTNode {
 
   toString() {
     if (!this.elseStatement) {
-      return `if (${this.ifStatement}) { ${this.thenStatement.join(' ')} }`;
+      return `if (${this.condStatement}) { ${this.thenStatement.join(' ')} }`;
     }
-    return `if (${this.ifStatement}) { ${this.thenStatement.join(' ')} } else { ${this.body} })`;
+    return `if (${this.condStatement}) { ${this.thenStatement.join(' ')} } else { ${this.body} })`;
+  }
+}
+
+//TODO: add a toString() method
+export class Assignment extends ASTNode {
+  constructor(from, to, operator, left, right, options={}) {
+    super(from, to, 'assignment', options);
+    this.operator = operator;
+    this.left = left;
+    this.right = right;
+  }
+
+  *[Symbol.iterator]() {
+    yield this;
+    for (let node of this.left) {
+      yield node;
+    }
+    for (let node of this.right) {
+      yield node;
+    }
+  }
+}
+
+//TODO: add a toString() method
+//is it possible to merge this somehow with assign class? Almost identical with it
+export class Binary extends ASTNode {
+  constructor(from, to, operator, left, right, options={}) {
+    super(from, to, 'binary', options);
+    this.operator = operator;
+    this.left = left;
+    this.right = right;
+  }
+
+  *[Symbol.iterator]() {
+    yield this;
+    for (let node of this.left) {
+      yield node;
+    }
+    for (let node of this.right) {
+      yield node;
+    }
+  }
+}
+
+//TODO: add a toString() method
+//use struct toString method as template for this toString() method?
+export class Prog extends ASTNode {
+  constructor(from, to, prog, options={}) {
+    super(from, to, 'prog', options);
+    this.prog = prog;
+  }
+
+  *[Symbol.iterator]() {
+    yield this;
+    for (let node of this.prog) {
+      yield node;
+    }
+  }
+}
+
+//TODO: add a toString() method
+export class Let extends ASTNode {
+  constructor(from, to, vars, body, options={}) {
+    super(from, to, 'let', options);
+    this.vars = vars;
+    this.body = body;
+  }
+
+  *[Symbol.iterator]() {
+    yield this;
+    for (let node of this.vars) {
+      yield node;
+    }
+    //why does body not need a for loop to be iterated over
+    yield this.body;
   }
 }
 
@@ -260,4 +334,3 @@ export class Blank extends ASTNode {
     return `${this.value}`;
   }
 }
-
