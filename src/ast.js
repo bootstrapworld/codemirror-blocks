6a0f3aa6dcb3643393dc2b2f66a4bc0246957914
Paul Carduner <paul@carduner.net>
2016-11-29 15:14:28 -0800

    Add navigation pointers

--- src/ast.js
+++ src/ast.js
@@ -224,6 +224,7 @@ export class CondClause extends ASTNode {
     super(from, to, 'condClause', options);
     this.testExpr = testExpr;
     this.thenExprs = thenExprs;
+    setNavigationPointers([testExpr, ...thenExprs], this)
   }
 
   *[Symbol.iterator]() {
@@ -241,6 +242,7 @@ export class CondExpression extends ASTNode {
   constructor(from, to, clauses, options={}) {
     super(from, to, 'condExpression', options);
     this.clauses = clauses;
+    setNavigationPointers(clauses, this)
   }
 
   *[Symbol.iterator]() {
