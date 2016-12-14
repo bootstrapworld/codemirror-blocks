b7a40665de484c015c864b91a76a04ac5f753f1d
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2016-12-14 08:46:09 -0500

    move reactification to non-master branch

---
+++ src/components/Blank.js
@@ -0,0 +1,26 @@
+import React, {Component, PropTypes} from 'react';
+
+import {Blank as ASTBlankNode} from '../ast';
+import Node from './Node';
+
+export default class Blank extends Component {
+  static propTypes = {
+    node: PropTypes.instanceOf(ASTBlankNode).isRequired
+  }
+  render() {
+    const {node} = this.props;
+    return (
+      <span type="literal"
+        className={`blocks-node blocks-literal`}
+        tabIndex="1"
+        role="treeitem"
+        aria-label={node.options['aria-label']}
+        id={`block-node-${node.id}`}
+      >
+        <span className={`blocks-literal-symbol`}>
+        ...
+        </span>
+      </span>
+    );
+  }
+}
