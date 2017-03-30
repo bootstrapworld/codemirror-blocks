ffda7fd081cfa3885651edc178d3a9247d97c712
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-03-30 07:50:29 -0400

    remove dead code

--- src/components/DropTarget.js
+++ src/components/DropTarget.js
@@ -1,15 +1,8 @@
-import React, {PureComponent, PropTypes} from 'react';
+import React, {PureComponent} from 'react';
 
 export default class DropTarget extends PureComponent {
-  static propTypes = {
-    location: PropTypes.shape({
-      line: PropTypes.number.isRequired,
-      ch: PropTypes.number.isRequired,
-    }).isRequired,
-  };
 
   render() {
-    const {location} = this.props;
     return (
       <span className="blocks-drop-target blocks-white-space" />
     );
