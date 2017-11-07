9a90bafff9ff00a160675087c8daf5760ab13627
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-11-06 22:16:18 -0500

    clean up condClause component

--- src/components/CondClause.js
+++ src/components/CondClause.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import {CondClause as ASTCondClauseNode} from '../ast';
+import Node from './Node';
 import DropTarget from './DropTarget';
 
 export default class CondClause extends Component {
@@ -16,16 +17,7 @@ export default class CondClause extends Component {
   render() {
     const {node, helpers, lockedTypes} = this.props;
     return (
-      <div 
-        type="condClause" 
-        className={`blocks-node blocks-${node.type}`}
-        tabIndex="1"
-        role="treeitem"
-        aria-label={node.options['aria-label']}
-        aria-expanded   = { node.collapsed ? "false" : "true" }
-        aria-level={node['aria-level']}
-        id={`block-node-${node.id}`}
-        ref = {(el) => node.el = el }>
+      <Node type="condClause" node={node} lockedTypes={lockedTypes} helpers={helpers}>
         <div className="blocks-cond-row">
           <div className="blocks-cond-predicate">
               <DropTarget location={node.testExpr.from} />
@@ -42,7 +34,7 @@ export default class CondClause extends Component {
         <div className="blocks-cond-drop-row">
           <DropTarget location={node.from} />
         </div>
-      </div>
+      </Node>
     );
   }
 }
