ec191a9a43316c72eb16dc3f2fdc490c7a087836
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-11-09 12:21:40 -0500

    fix hound errors

--- src/components/IfExpression.js
+++ src/components/IfExpression.js
@@ -20,25 +20,25 @@ export default class IfExpression extends Component {
       <Node type="ifExpression" node={node} lockedTypes={lockedTypes} helpers={helpers}>
         <span className="blocks-operator">if</span>
         <div className="blocks-cond-table">
-            <div className="blocks-cond-row">
-              <div className="blocks-cond-predicate">
-                <DropTarget location={node.testExpr.from} />
-                {helpers.renderNodeForReact(node.testExpr)}
-              </div>
-              <div className="blocks-cond-result">
-                <DropTarget location={node.thenExpr.from} />
-                {helpers.renderNodeForReact(node.thenExpr)}
-              </div>
+          <div className="blocks-cond-row">
+            <div className="blocks-cond-predicate">
+              <DropTarget location={node.testExpr.from} />
+              {helpers.renderNodeForReact(node.testExpr)}
             </div>
-            <div className="blocks-cond-row">
-              <div className="blocks-cond-predicate blocks-cond-else">
-                else
-              </div>
-              <div className="blocks-cond-result">
-                <DropTarget location={node.elseExpr.from} />
-                {helpers.renderNodeForReact(node.elseExpr)}
-              </div>
+            <div className="blocks-cond-result">
+              <DropTarget location={node.thenExpr.from} />
+              {helpers.renderNodeForReact(node.thenExpr)}
             </div>
+          </div>
+          <div className="blocks-cond-row">
+            <div className="blocks-cond-predicate blocks-cond-else">
+              else
+            </div>
+            <div className="blocks-cond-result">
+              <DropTarget location={node.elseExpr.from} />
+              {helpers.renderNodeForReact(node.elseExpr)}
+            </div>
+          </div>
         </div>
       </Node>
     );
