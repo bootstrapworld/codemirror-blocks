65ffddfbb4161cf7dd715bace992e0ce2a3f4858
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-12-26 21:54:24 -0500

    add support for let-like expressions, and (experimentally) remove castToASTNode() from patching

---
+++ src/languages/wescheme/ast.js
@@ -0,0 +1,25 @@
+import {ASTNode, pluralize, descDepth} from '../../ast';
+
+export class LetLikeExpr extends ASTNode {
+  constructor(from, to, form, bindings, expr, options={}) {
+    super(from, to, 'letLikeExpr', options);
+    this.form = form;
+    this.bindings = bindings;
+    this.expr = expr;
+  }
+
+  *[Symbol.iterator]() {
+    yield this;
+    yield this.bindings;
+    yield this.expr;
+  }
+
+  toDescription(level){
+    if((this['aria-level']- level) >= descDepth) return this.options['aria-label'];
+    return `a ${this.form} expression with ${pluralize("binding", this.bindings.exprs)}`
+  }
+
+  toString() {
+    return `(${this.form} (${this.bindings.toString()}) ${this.expr}`;
+  }
+}
---
+++ src/languages/wescheme/components/LetLikeExpr.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {Sequence as ASTSequenceNode} from '../../../ast';
+import {LetLikeExpr as ASTLetLikeExprNode} from '../ast';
+import Node from '../../../components/Node';
+import DropTarget from '../../../components/DropTarget';
+
+export default class LetLikeExpr extends Component {
+  static propTypes = {
+    node: PropTypes.instanceOf(ASTLetLikeExprNode).isRequired,
+    helpers: PropTypes.shape({
+      renderNodeForReact: PropTypes.func.isRequired,
+    }).isRequired,
+    lockedTypes: PropTypes.instanceOf(Array).isRequired,
+  }
+
+  render() {
+    const {node, helpers, lockedTypes} = this.props;
+    return (
+      <Node type="letLikeExpr" node={node} lockedTypes={lockedTypes} helpers={helpers}>
+        <span className="blocks-operator">{node.form}</span>
+        {helpers.renderNodeForReact(node.bindings)}
+        {helpers.renderNodeForReact(node.expr)}
+      </Node>
+    );
+  }
+}
