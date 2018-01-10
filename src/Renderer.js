ed4e41f169de81884ac85c273ae2d3ed04ee0429
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-01-10 18:34:48 -0500

    make it possible to render non-root nodes

--- src/Renderer.js
+++ src/Renderer.js
@@ -99,7 +99,7 @@ export default class Renderer {
     function flatten(flat, node) {
       return ["literal", "blank"].includes(node.type)? flat.concat([node])  // nothing inside literals and blanks
           : that.lockNodesOfType.includes(node.type)? flat.concat([node])   // Perf: don't bother looking inside
-          : [...node].slice(1).reduce(flatten, flat);                       // look inside
+            : [...node].slice(1).reduce(flatten, flat);                      // look inside
     }
 
     // 1) Limit the number of lines CM is rendering (perf), and extract visible nodes, & make clones 
@@ -142,19 +142,25 @@ export default class Renderer {
   }
 
   // Render the rootNode into a new marker, clearing any old ones
-  render(rootNode, quarantine=false) {
-    var marker = this.cm.findMarksAt(rootNode.from).filter(m => m.node)[0];
-    // recycle the container, if we can
-    let container = (marker && !quarantine)? marker.replacedWith : document.createElement('span');
-    if(marker && !quarantine) marker.clear();
-    this.cm.markText(rootNode.from, rootNode.to, {replacedWith: container, node: rootNode} );
-    
-    // REVISIT: make comments disappear by adding an empty span
-    if(rootNode.options.comment) {
-      this.cm.markText(rootNode.options.comment.from, rootNode.options.comment.to,
-        { replacedWith: document.createElement('span') });
+  // Render the node, recycling a container whenever possible
+  render(node, quarantine=false) {
+    var container;
+    if(node["aria-level"] == 1) { // if it's a root node, reset the marker but save the container
+      let marker = this.cm.findMarksAt(node.from).filter(m => m.node)[0];
+      // recycle the container, if we can
+      container = (marker && !quarantine)? marker.replacedWith : document.createElement('span');
+      if(marker && !quarantine) marker.clear();
+      this.cm.markText(node.from, node.to, {replacedWith: container, node: node} );
+      
+      // REVISIT: make comments disappear by adding an empty span
+      if(node.options.comment) {
+        this.cm.markText(node.options.comment.from, node.options.comment.to,
+          { replacedWith: document.createElement('span') });
+      }
+    } else { // otherwise just render in-place
+      container = node.el.parentNode;
     }
-    ReactDOM.render(this.renderNodeForReact(rootNode), container);
+    ReactDOM.render(this.renderNodeForReact(node), container);
     container.className = 'react-container';
     return container;
   }
