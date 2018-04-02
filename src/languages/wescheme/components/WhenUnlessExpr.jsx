dc63bbffa2b3df9513693a188bb2cbafca230553
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-04-02 12:01:31 -0400

    unclear how this file was missed

---
+++ src/languages/wescheme/components/WhenUnlessExpr.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {WhenUnless as ASTWhenUnlessNode} from '../ast';
+import Node from '../../../components/Node';
+
+export default class WhenUnlessExpr extends Component {
+  static propTypes = {
+    node: PropTypes.instanceOf(ASTWhenUnlessNode).isRequired,
+    helpers: PropTypes.shape({
+      renderNodeForReact: PropTypes.func.isRequired,
+    }).isRequired,
+    lockedTypes: PropTypes.instanceOf(Array).isRequired,
+  }
+
+  render() {
+    const {node, helpers, lockedTypes} = this.props;
+    return (
+      <Node node={node} lockedTypes={lockedTypes} helpers={helpers}>
+        <span className="blocks-operator">{node.form}</span>
+        {helpers.renderNodeForReact(node.predicate)}
+        {helpers.renderNodeForReact(node.exprs)}
+      </Node>
+    );
+  }
+}
