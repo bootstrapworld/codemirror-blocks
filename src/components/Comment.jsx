536a8232804c6ab3c50d928bdbf550762cbdb4c6
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-05-02 17:46:22 -0400

    fix rendering of comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -10,7 +10,7 @@ export default class Comment extends PureComponent {
   }
   render() {
     const {node} = this.props;
-    return (<span className="blocks-comment" id={node.id}>
+    return (<span className="blocks-comment" id={node.id} aria-hidden="true">
       <span className="sr-only"> Comment,</span> {node.comment.toString()}
     </span>);
   }
