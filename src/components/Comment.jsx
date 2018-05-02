e6300d6580b8b23c765826589b97c8d3bd86015b
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-05-02 17:46:22 -0400

    merge if-exp and comment-rendering fix

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -10,8 +10,8 @@ export default class Comment extends PureComponent {
   }
   render() {
     const {node} = this.props;
-    return (<span className="blocks-comment" id={node.id}>
-      {node.comment.toString()}
+    return (<span className="blocks-comment" id={node.id} aria-hidden="true">
+      <span className="sr-only"> Comment,</span> {node.comment.toString()}
     </span>);
   }
 }
