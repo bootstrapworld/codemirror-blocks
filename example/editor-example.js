bf3ad3ed3ea5d9847be9ee7ded44923aa499a53c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2018-08-09 02:48:51 +0700

    Get search working with editor.html and reactify editor.html
    Note that this reactify is orthogonal to the reactify branch.

--- example/editor-example.js
+++ example/editor-example.js
@@ -5,64 +5,79 @@ import 'codemirror/addon/search/searchcursor.js';
 import '../src/languages/wescheme';
 import '../src/languages/example';
 import '../src/languages/lambda';
-import {renderEditorInto} from '../src/ui';
 import CodemirrorBlocks from '../src/blocks.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Editor from '../src/ui/Editor';
+import './example-page.less';
+import exampleWeSchemeCode from './ast-test.rkt';
 
-require('./example-page.less');
-
-var code = require('./ast-test.rkt');
-
-const options = {
-  renderOptions: {
-    lockNodesOfType: ['comment','functionDef','variableDef','struct']
-  },
-  toolbar: document.getElementById('toolbar'),
-  willInsertNode(sourceNodeText, sourceNode, destination) {
-    let line = editor.getCodeMirror().getLine(destination.line);
-    let prev = line[destination.ch - 1] || '\n';
-    let next = line[destination.ch] || '\n';
-    sourceNodeText = sourceNodeText.trim();
-    if (!/\s|[\(\[\{]/.test(prev)) {
-      sourceNodeText = ' ' + sourceNodeText;
-    }
-    if (!/\s|[\)\]\}]/.test(next)) {
-      sourceNodeText += ' ';
-    }
-    return sourceNodeText;
-  }
+const exampleCodes = {
+  wescheme: exampleWeSchemeCode,
+  example: "1",
+  lambda: "2",
 };
 
-let editor = renderEditorInto(
-  document.getElementById('editor'),
-  'wescheme',
-  options
-);
-editor.getCodeMirror().setValue(code);
-editor.getCodeMirror().setOption("lineNumbers", true);
-editor.getCodeMirror().setOption("viewportMargin", 10);
-editor.getCodeMirror().doc.clearHistory();
+class EditorInstance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.options = {
+      renderOptions: {
+        lockNodesOfType: ['comment', 'functionDef', 'variableDef', 'struct']
+      },
+      willInsertNode: (sourceNodeText, sourceNode, destination) => {
+        // TODO: the need of this.ref here indicates that `willInsertNode` has
+        // a bad interface. Perhaps it should also provide editor
 
-ReactDOM.render((
-  <select onChange={function(event){
-    ReactDOM.unmountComponentAtNode(document.getElementById('editor'));
-    var editor = renderEditorInto(
-        document.getElementById('editor'),
-        event.target.value,
-        options
+        const line = this.ref.current.getCodeMirror().getLine(destination.line);
+        const prev = line[destination.ch - 1] || '\n';
+        const next = line[destination.ch] || '\n';
+        sourceNodeText = sourceNodeText.trim();
+        if (!/\s|[([{]/.test(prev)) {
+          sourceNodeText = ' ' + sourceNodeText;
+        }
+        if (!/\s|[)\]}]/.test(next)) {
+          sourceNodeText += ' ';
+        }
+        return sourceNodeText;
+      }
+    };
+    this.state = {language: 'wescheme'};
+  }
+
+  getCmOptions() {
+    return {
+      value: exampleCodes[this.state.language],
+      lineNumbers: true,
+      viewportMargin: 10,
+    };
+  }
+
+  handleChange = event => {
+    this.ref.current.getCodeMirror().doc.clearHistory();
+    this.setState({language: event.target.value});
+  }
+
+  render() {
+    const choices = CodemirrorBlocks.languages.getLanguages().map(
+      language => (
+        <option value={language.id} key={language.id}>{language.name}</option>
+      )
     );
-    var exampleCode = CodemirrorBlocks.languages.getLanguage(event.target.value).example;
-    editor.getCodeMirror().setValue(exampleCode || "");
-  }}>
-    <option>
-      Choose Language...
-    </option>
-    {CodemirrorBlocks.languages.getLanguages().map(
-       language => (
-         <option value={language.id} key={language.id}>{language.name}</option>
-       )
-     )}
-  </select>
-),document.getElementById('language-selector')
-);
+    return (
+      <React.Fragment>
+        <select value={this.state.language} onChange={this.handleChange}
+                id="language-chooser">
+          {choices}
+        </select>
+        <Editor ref={this.ref}
+                language={this.state.language}
+                options={this.options}
+                cmOptions={this.getCmOptions()} />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<EditorInstance />, document.getElementById('editor'));
