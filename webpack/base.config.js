90f6cf303636d8d3ddaa62cc74dabf783a4336b6
Justin <zallambo@gmail.com>
2018-08-16 19:26:09 -0400

    fix: Should actually match the dots

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -5,9 +5,9 @@ module.exports = function(config) {
   config = config || {};
   var plugins = [];
   var rules = [
-    {test:/.png$|.jpg$|.jpeg$|.gif$|.svg$|.wav$/, use: "url-loader?limit=10000"},
-    {test:/.woff$|.woff2$/, use: "url-loader?limit=10000"},
-    {test:/.ttf$|.eot$/, use: "file-loader"},
+    {test:/\.png$|\.jpg$|\.jpeg$|\.gif$|\.svg$|\.wav$/, use: "url-loader?limit=10000"},
+    {test:/\.woff$|\.woff2$/, use: "url-loader?limit=10000"},
+    {test:/\.ttf$|\.eot$/, use: "file-loader"},
   ];
   if (config.extractCSS) {
     plugins.push(new ExtractTextPlugin("[name].css"));
