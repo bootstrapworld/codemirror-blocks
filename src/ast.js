bootstrapworld/codemirror-blocks

commit a3caf9135f3edd69a922ede8a7c1e89adfbb3963

always fallback to closestNodeFromPath, since an edit may have changed the tree

src/ast.js
@@ -61,25 +61,23 @@ function markChanges(oldAST, newAST, changes) {
 // and a set of nodes that must be re-rendered
 export function patch(oldAST, newAST, CMchanges) {
   markChanges(oldAST, newAST, CMchanges); // mark dirty, deleted and inserted nodes as such
-  let newNodes = [...newAST.nodeIdMap.values()], newIdx = 0;
+  let newNodes = [...newAST.nodeIdMap.values()], newIdx = -1;
   let oldNodes = [...oldAST.nodeIdMap.values()];
   
-  // walk through the nodes, unifying those that are unchanged
+  // walk through the nodes, unifying those that weren't deleted or inserted
   oldNodes.forEach(oldNode => {
-    let newNode = newNodes[newIdx];
-    // (1) Read ahead to the 1st non-inserted newNode. (2) If we're out of newNodes
-    // or the oldNode was deleted, return and iterate over the next oldNode. 
+    let newNode = newNodes[++newIdx];
+    // (1) Read ahead to the 1st non-inserted newNode. 
     while(newNode && newNodes[newIdx].inserted) { newNode = newNodes[++newIdx];  }
+    // (2) If we're out of newNodes or the oldNode was deleted, process next oldNode. 
     if(!newNode || oldNode.deleted) { return; }
-    // These are matching nodes -- unify properties
-    newNodes[newIdx].id     = oldNode.id;       // for react reconciliation later
-    newNodes[newIdx].el     = oldNode.el;       // for OLD DRAWING CODE
-    newNodes[newIdx].dirty |= oldNode.dirty;    // for OLD DRAWING CODE
-    newIdx++;
+    // These are matching nodes -- unify properties (for new-reactify, only copy ID)
+    newNodes[newIdx].id     = oldNode.id;
+    newNodes[newIdx].el     = oldNode.el;       // delete for new-reactify
+    newNodes[newIdx].dirty |= oldNode.dirty;    // delete for new-reactify
   });
   newAST.dirtyNodes = removeChildren(new Set(newNodes.filter(n => n.dirty)));
   newAST.annotateNodes();
-  console.log(...newAST.dirtyNodes);
   return newAST;
 }
 

src/blocks.js
@@ -176,9 +176,11 @@ export default class CodeMirrorBlocks {
       setTimeout(() => this.mouseUsed = false, 200);
     });
     // override CM's natural onFocus behavior, activating the last focused node
+    // this.focusPath is *not guaranteed to exist*, since an edit may have occurred
     // skip this if it's the result of a mousedown event
     this.cm.on('focus',     (cm, e) => {
       if(this.blockMode && this.ast.rootNodes.length > 0 && !this.mouseUsed) {
+        this.focusPath = this.ast.getClosestNodeFromPath(this.focusPath.split(',')).path;
         setTimeout(() => { this.activateNode(this.ast.getNodeByPath(this.focusPath), e); }, 10);
       }
     });

commit ab7249445df93fde49ff35d8b8ced575f6091ee6

fix patching bug introduced in a3caf9135f3edd69a922ede8a7c1e89adfbb3963

src/ast.js
@@ -67,12 +67,12 @@ function markChanges(oldAST, newAST, changes) {
 // and a set of nodes that must be re-rendered
 export function patch(oldAST, newAST, CMchanges) {
   markChanges(oldAST, newAST, CMchanges); // mark dirty, deleted and inserted nodes as such
-  let newNodes = [...newAST.nodeIdMap.values()], newIdx = -1;
+  let newNodes = [...newAST.nodeIdMap.values()], newIdx = 0;
   let oldNodes = [...oldAST.nodeIdMap.values()];
   
   // walk through the nodes, unifying those that weren't deleted or inserted
   oldNodes.forEach(oldNode => {
-    let newNode = newNodes[++newIdx];
+    let newNode = newNodes[newIdx];
     // (1) Read ahead to the 1st non-inserted newNode. 
     while(newNode && newNodes[newIdx].inserted) { newNode = newNodes[++newIdx];  }
     // (2) If we're out of newNodes or the oldNode was deleted, process next oldNode. 
@@ -81,6 +81,7 @@ export function patch(oldAST, newAST, CMchanges) {
     newNodes[newIdx].id     = oldNode.id;
     newNodes[newIdx].el     = oldNode.el;       // delete for new-reactify
     newNodes[newIdx].dirty |= oldNode.dirty;    // delete for new-reactify
+    newIdx++;
   });
   newAST.dirtyNodes = removeChildren(new Set(newNodes.filter(n => n.dirty)));
   newAST.annotateNodes();