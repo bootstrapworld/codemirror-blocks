6e6ce6cb6b3d7355ab0871b3b31b2016e43f0eeb
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-11-12 12:41:57 -0500

    implement keymaps

---
+++ src/keymap.js
@@ -0,0 +1,44 @@
+import { ISMAC } from './keycode';
+
+export let keyMap = {
+  "Up":         "findPrevVisibleNode",
+  "Down":       "findNextVisibleNode",
+  "Left":       "collapseOrFindParent",
+  "Right":      "expandOrFindFirstChild",
+  "Home":       "activateFirstVisibleNode",
+  "End":        "activateLastVisibleNode",
+  "Space":      "toggleSelection",
+  "Enter":      "editOrToggleExpanded",
+  "\\":         "speakChildren",
+  "Shift-\\":   "speakParents",
+  "Ctrl-[":     "insertToLeft",
+  "Ctrl-]":     "insertToRight",
+  "Shift-Left": "collapseAll",
+  "Shift-Right":"expandAll",
+  "Delete":     "deleteSelectedNodes",
+  "Backspace":  "deleteSelectedNodes",
+  "Ctrl-Delete":"deleteSelectedNodes",
+  "Ctrl-Backspace":"deleteSelectedNodes",
+  "Shift-,":     "activateRoot",
+  "Shift-9":     "insertEmptyExpression"
+};
+
+if(ISMAC) {
+  keyMap["Alt-Space"] = "toggleSelectionAndPreserveSelection";
+  keyMap["Alt-Down"]  = "findNextVisibleNodeAndPreserveSelection";
+  keyMap["Alt-Up"]    = "findPrevVisibleNodeAndPreserveSelection";
+  keyMap["Cmd-Enter"] = "editNode";
+  keyMap["Cmd-Z"]     = "undo";
+  keyMap["Shift-Cmd-Z"]="redo";
+  keyMap["Cmd-V"]     = "paste";
+  keyMap["Shift-Cmd-V"]="paste";
+} else {
+  keyMap["Ctrl-Space"] = "toggleSelectionAndPreserveSelection";
+  keyMap["Ctrl-Down"]  = "findNextVisibleNodeAndPreserveSelection";
+  keyMap["Ctrl-Up"]    = "findPrevVisibleNodeAndPreserveSelection";
+  keyMap["Ctrl-Enter"] = "editNode";
+  keyMap["Ctrl-Z"]     = "undo";
+  keyMap["Ctrl-Y"]     = "redo";
+  keyMap["Ctrl-V"]     = "paste";
+  keyMap["Shift-Ctrl-V"]="paste";
+}
