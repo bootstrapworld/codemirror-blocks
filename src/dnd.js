0eb77226a0f71e9981f489f183cb2286270ba88f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-01-03 12:25:27 -0500

    make dropping on a non-target into a beeping no-op (partially addressess #172)
    Conflicts:
    	src/dnd.js

--- src/dnd.js
+++ src/dnd.js
@@ -1,6 +1,7 @@
 import {DragSource, DropTarget} from 'react-dnd';
 import {isDummyPos} from './utils';
 import global from './global';
+import {playSound, BEEP} from './sound';
 
 export const ItemTypes = {
   NODE: 'node',
@@ -19,7 +20,8 @@ export const nodeSource = {
 export function collectSource(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging()
+    isDragging: monitor.isDragging(),
+    connectDragPreview: connect.dragPreview()
   };
 }
 
@@ -27,12 +29,23 @@ export const nodeTarget = nodeTargetFn => ({
   drop(props, monitor) {
     if (monitor.didDrop()) return;
     const {x: left, y: top} = monitor.getClientOffset();
-    props.onDrop(
-      monitor.getItem(),
-      nodeTargetFn(props.location
-                   ? props
-                   : {...props, location: global.cm.coordsChar({left, top})})
-    );
+    // if it's a proper drop target, just drop at the provided location
+    if(props.location) {
+      return props.onDrop(monitor.getItem(), nodeTargetFn(props));
+    }
+    // if not, make sure it's a valid drop (i.e. - not on any CM node marker)
+    let roots = global.cm.getAllMarks().filter(m => m.BLOCK_NODE_ID);
+    let validTopLevelDrop = !roots.some(root => {
+      let r = root.replacedWith.firstChild.getBoundingClientRect();
+      return (r.left<left) && (left<r.right) && (r.top<top) && (top<r.bottom);
+    });
+    if(validTopLevelDrop) {
+      props.onDrop(
+        monitor.getItem(),
+        nodeTargetFn({...props, location: global.cm.coordsChar({left, top})}));
+    } else { // beep and make it a no-op
+      playSound(BEEP);
+    }
   }
 });
 
