e7f3d78476f2a853c1b7b14c7d82e2ffceb81a7e
CaoRuiming <rcao22556@gmail.com>
2019-01-15 10:39:03 -0500

    added enzyme for tests

--- src/Renderer.js
+++ src/Renderer.js
@@ -160,7 +160,6 @@ export default class Renderer {
         />
       );
     } else {
-      console.log('vv Error:', node.type, this.extraRenderers, Renderer);
       throw new Error("Don't know how to render node of type: "+node.type);
     }
   }
