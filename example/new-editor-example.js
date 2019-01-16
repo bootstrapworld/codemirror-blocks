134fb46b37e6089c02475b1152de7c01fc095260
Justin Pombrio <justinpombrio@cs.brown.edu>
2019-01-16 13:36:13 -0500

    test: switch to bigger example code for manual testing

--- example/new-editor-example.js
+++ example/new-editor-example.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import wescheme from '../src/languages/wescheme';
 import ToggleEditor from '../src/ui/ToggleEditor';
 import './example-page.less';
+import bigExampleCode from './ast-test.rkt';
 
 
-const exampleCode = `(+ 1 2) ;comment\n(+ 3 4)`;
+const smallExampleCode = `(+ 1 2) ;comment\n(+ 3 4)`;
+
+const useBigCode = true;
+const exampleCode = useBigCode ? bigExampleCode : smallExampleCode;
 
 ReactDOM.render(<ToggleEditor language={wescheme} initialCode={exampleCode}/>,
                 document.getElementById('cmb-editor'));
