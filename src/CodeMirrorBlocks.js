339a6fdb85596cf73984d84921a3b956ca372a5c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-01-23 10:46:48 -0500

    add missing file

--- src/CodeMirrorBlocks.js
+++ src/CodeMirrorBlocks.js
@@ -6,21 +6,13 @@ import wescheme from '../src/languages/wescheme';
 // Consumes a DOM node to host the editor, a language object and the code
 // to render. Produces an object-representation of CMB, allowing for
 // integration with external (non-react) code
-export default class CodeMirrorBlocks {
-  constructor(container, language = wescheme, code = "", options = {}) {
+export class CodeMirrorBlocks {
+  constructor(dom, language = wescheme, code = "") {
     let obj = {};
     ReactDOM.render(
-      <ToggleEditor
-        language={language}
-        initialCode={code}
-        external={obj}
-        appElement={container}
-        options={options}
-      />,
-      container
+      <ToggleEditor language={language} initialCode={code} external={obj} />,
+      dom
     );
     return obj;
   }
 }
-
-module.exports = CodeMirrorBlocks;
