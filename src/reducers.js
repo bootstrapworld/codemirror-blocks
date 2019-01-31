d2a413d0fb4b5c269df957bfece6d5b64bf0a2a1
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-01-31 16:08:08 -0500

    update the state.collapsedList whenever the AST changes, to make sure every collapsed ID really exists

--- src/reducers.js
+++ src/reducers.js
@@ -17,7 +17,8 @@ export const reducer = (
     case 'SET_FOCUS':
       return {...state, focusId: action.focusId};
     case 'SET_AST':
-      return {...state, ast: action.ast};
+      return {...state, ast: action.ast, collapsedList: state.collapsedList.filter(
+        c => action.ast.getNodeById(c))};
     case 'SET_SELECTIONS':
       return {...state, selections: action.selections};
     case 'SET_ERROR_ID':
