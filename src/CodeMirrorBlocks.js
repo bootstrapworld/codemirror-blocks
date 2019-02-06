2df88477613d4c49ecf3742f0c04d0f86c2f9ea8
CaoRuiming <rcao22556@gmail.com>
2019-02-06 11:55:07 -0500

    updated CodeMirrorBlocks.js to match new-reactify

--- src/CodeMirrorBlocks.js
+++ src/CodeMirrorBlocks.js
@@ -6,13 +6,21 @@ import wescheme from '../src/languages/wescheme';
 // Consumes a DOM node to host the editor, a language object and the code
 // to render. Produces an object-representation of CMB, allowing for
 // integration with external (non-react) code
-export class CodeMirrorBlocks {
-  constructor(dom, language = wescheme, code = "") {
+export default class CodeMirrorBlocks {
+  constructor(container, language = wescheme, code = "", options = {}) {
     let obj = {};
     ReactDOM.render(
-      <ToggleEditor language={language} initialCode={code} external={obj} />,
-      dom
+      <ToggleEditor
+        language={language}
+        initialCode={code}
+        external={obj}
+        appElement={container}
+        options={options}
+      />,
+      container
     );
     return obj;
   }
 }
+
+module.exports = CodeMirrorBlocks;
