fcb1c625d28c84830b58b9f62faffe6ac6f82db6
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-02-27 16:59:22 -0500

    lint

--- src/ui/BlockEditor.js
+++ src/ui/BlockEditor.js
@@ -353,43 +353,43 @@ class BlockEditor extends Component {
     let withState = (func) => this.props.dispatch((_, getState) => func(getState()));
     return {
       // cm methods
-        'markText':   (from, to, opts) => this.markText(from, to, opts),
-        'findMarks':  (from, to) => this.findMarks(from, to),
-        'findMarksAt':(pos) => this.findMarksAt(pos),
-        'getAllMarks':() => this.getAllMarks(),
-        'getValue': (sep) => ed.getValue(sep),
-        'setValue': (value) => ed.setValue(value),
-        'getScrollerElement': () => ed.getScrollerElement(),
-        'getWrapperElement': () => ed.getWrapperElement(),
-        'getInputField': () => ed.getInputField(),
-        'getCursor': (start) => ed.getCursor(start),
-        'replaceRange': ed.replaceRange,
-        'setCursor': (pos) => this.props.setCursor(ed, pos),
-        'runMode': (_src, _lang, _container) => () => {}, // no-op since not an editing command
-        'refresh': () => ed.refresh(),
-        'defineOption': (name, _default, updateFunc) => ed.defineOption(name, _default, updateFunc),
-        'Pos': (line, ch, sticky) => ed.Pos(line, ch, sticky),
-        'Doc': (text, mode, firstLineNumber, lineSeparator) => ed.Doc(text, mode, firstLineNumber, lineSeparator),
-        'swapDoc': (doc) => ed.swapDoc(doc),
-        'getDoc': () => ed.getDoc(),
-        'charCoords': (pos, mode) => ed.charCoords(pos, mode),
-        'getScrollInfo': () => ed.getScrollInfo(),
-        'getWrapperElement': () => ed.getWrapperElement(),
-        'scrollIntoView': (what, margin) => ed.scrollIntoView(what, margin),
-        'addLineClass': (line, where, _class) => ed.addLineClass(line, where, _class),
-        'on': (type, func) => ed.on(type, func), // another on(obj, type, func) version...
-        'off': (type, func) => ed.off(type, func),
-        'removeLineClass': (line, where, _class) => ed.removeLineClass(line, where, _class),
+      'markText':   (from, to, opts) => this.markText(from, to, opts),
+      'findMarks':  (from, to) => this.findMarks(from, to),
+      'findMarksAt':(pos) => this.findMarksAt(pos),
+      'getAllMarks':() => this.getAllMarks(),
+      'getValue': (sep) => ed.getValue(sep),
+      'setValue': (value) => ed.setValue(value),
+      'getScrollerElement': () => ed.getScrollerElement(),
+      'getWrapperElement': () => ed.getWrapperElement(),
+      'getInputField': () => ed.getInputField(),
+      'getCursor': (start) => ed.getCursor(start),
+      'replaceRange': ed.replaceRange,
+      'setCursor': (pos) => this.props.setCursor(ed, pos),
+      'runMode': (_src, _lang, _container) => () => {}, // no-op since not an editing command
+      'refresh': () => ed.refresh(),
+      'defineOption': (name, _default, updateFunc) => ed.defineOption(name, _default, updateFunc),
+      'Pos': (line, ch, sticky) => ed.Pos(line, ch, sticky),
+      'Doc': (text, mode, firstLineNumber, lineSeparator) => ed.Doc(text, mode, firstLineNumber, lineSeparator),
+      'swapDoc': (doc) => ed.swapDoc(doc),
+      'getDoc': () => ed.getDoc(),
+      'charCoords': (pos, mode) => ed.charCoords(pos, mode),
+      'getScrollInfo': () => ed.getScrollInfo(),
+      'getWrapperElement': () => ed.getWrapperElement(),
+      'scrollIntoView': (what, margin) => ed.scrollIntoView(what, margin),
+      'addLineClass': (line, where, _class) => ed.addLineClass(line, where, _class),
+      'on': (type, func) => ed.on(type, func), // another on(obj, type, func) version...
+      'off': (type, func) => ed.off(type, func),
+      'removeLineClass': (line, where, _class) => ed.removeLineClass(line, where, _class),
       // block methods
-        'getAst':
-          () => withState((state) => state.ast),
-        'getFocusedNode':
-          () => withState(({focusId, ast}) => focusId ? ast.getNodeById(focusId) : null),
-        'getSelectedNodes':
-          () => withState(({selections, ast}) => selections.map(id => ast.getNodeById(id))),
+      'getAst':
+        () => withState((state) => state.ast),
+      'getFocusedNode':
+        () => withState(({focusId, ast}) => focusId ? ast.getNodeById(focusId) : null),
+      'getSelectedNodes':
+        () => withState(({selections, ast}) => selections.map(id => ast.getNodeById(id))),
       // testing methods
-        'getQuarantine': () => withState(({quarantine}) => quarantine),
-        'setQuarantine': (q) => this.props.setQuarantine(q),
+      'getQuarantine': () => withState(({quarantine}) => quarantine),
+      'setQuarantine': (q) => this.props.setQuarantine(q),
     };
   }
 
