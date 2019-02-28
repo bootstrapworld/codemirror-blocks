ea5694893735030c9ea1b09f04941977228aedbb
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2019-02-28 14:47:47 -0500

    try to work on a fromTextArea thing

--- src/CodeMirrorBlocks.js
+++ src/CodeMirrorBlocks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToggleEditor from './ui/ToggleEditor';
 import wescheme from '../src/languages/wescheme';
+import merge from './merge';
 
 // Consumes a DOM node to host the editor, a language object and the code
 // to render. Produces an object-representation of CMB, allowing for
@@ -19,7 +20,20 @@ export default class CodeMirrorBlocks {
       />,
       container
     );
+    merge(api, this.buildAPI());
     return api;
+  }
+
+  buildAPI = () => {
+    return {
+      'fromTextArea': this.fromTextArea,
+    }
+  }
+
+  fromTextArea = myTextArea => {
+    myTextArea.parentNode.replaceChild(<ToggleEditor
+      initialCode={myTextArea.value}
+    />, myTextArea);
   }
 }
 
