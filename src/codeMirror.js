bootstrapworld/codemirror-blocks

commit 443f8805092d417c49670ad265c933ced6f926c0

small tweaks

src/codeMirror.js
@@ -26,7 +26,7 @@ export function commitChanges(
     // patch the tree and set the state
     SHARED.cm.operation(changes(SHARED.cm));
     let {ast: oldAST, collapsedList} = store.getState();
-    let lastChange = changeArr[0], dragId, dragTo, focusId;
+    let lastChange = changeArr[0], dragId, dragTo;
     // walk through the change array to look for drag events, defined as
     // consecutive changes where identical text is removed in one and inserted
     // in the other. For each one, perform an interim patch operation using
@@ -51,10 +51,12 @@ export function commitChanges(
     // if there are still (non-DnD) changes to be patched, do so
     if(oldAST.hash !== newAST.hash) newAST = patch(oldAST, newAST);
     let focusNode = computeFocusNodeFromChanges(changeArr, newAST);
+    let focusId = focusNode.id;
     store.dispatch({type: 'SET_AST', ast: newAST});
     while(focusNode.parent && (focusNode = focusNode.parent)) {
       if(collapsedList.includes(focusNode.id)) focusId = focusNode.id;
     }
+    console.log('focusing on', focusId);
     store.dispatch(activate(focusId));
     onSuccess({newAST, focusId});
   };

src/less/default-style.less
@@ -197,7 +197,8 @@
   border-radius: 5px;
   padding: 0px 2px;
   border: @border-width solid transparent;
-  min-width: 10px;
+  min-width: 15px;
+  text-align: center;
   line-height: @height - @border-width * 2;
   height: @height;
   font-size: 12px;

commit 65b3ddba34e0cab312a9d8a429f803654df755e9

fix regression introduced in 443f8805092d417c49670ad265c933ced6f926c0

src/codeMirror.js
@@ -51,12 +51,11 @@ export function commitChanges(
     // if there are still (non-DnD) changes to be patched, do so
     if(oldAST.hash !== newAST.hash) newAST = patch(oldAST, newAST);
     let focusNode = computeFocusNodeFromChanges(changeArr, newAST);
-    let focusId = focusNode.id;
+    let focusId = focusNode? focusNode.id : null;
     store.dispatch({type: 'SET_AST', ast: newAST});
-    while(focusNode.parent && (focusNode = focusNode.parent)) {
+    while(focusNode && focusNode.parent && (focusNode = focusNode.parent)) {
       if(collapsedList.includes(focusNode.id)) focusId = focusNode.id;
     }
-    console.log('focusing on', focusId);
     store.dispatch(activate(focusId));
     onSuccess({newAST, focusId});
   };