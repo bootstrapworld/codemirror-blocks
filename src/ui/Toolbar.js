7c8e22ab81b7ac90680107b707db4f0161d89745
Justin Pombrio <justinpombrio@cs.brown.edu>
2019-04-11 20:19:15 -0400

    feat: Up and down arrows navigate toolbar after searching. Fixes #235.

--- src/ui/Toolbar.js
+++ src/ui/Toolbar.js
@@ -5,6 +5,7 @@ import PrimitiveList from './PrimitiveList';
 import PrimitiveBlock from './PrimitiveBlock';
 import {PrimitiveGroup} from '../parsers/primitives';
 import './Toolbar.less';
+import SHARED from '../shared';
 
 export default class Toolbar extends Component {
   constructor(props) {
@@ -34,11 +35,22 @@ export default class Toolbar extends Component {
     this.setState({search: ''});
   }
 
-  checkEscape = (event) => {
-    if (event.key == 'Escape') {
-      event.target.blur();
-      event.preventDefault();
-    }
+  next() {
+    let primitives = this.getPrimitives();
+    if (primitives.length == 0) return; // Nothing to select.
+    let i = this.getSelectedPrimitiveIndex(primitives);
+    // If nothing is selected, select the first primitive, otherwise select the next.
+    if (i === primitives.length - 1) i -= 1;
+    if (i === null) i = -1;
+    this.setState({selectedPrimitive: primitives[i + 1]});
+  }
+
+  prev() {
+    let primitives = this.getPrimitives();
+    if (primitives.length == 0) return; // Nothing to select.
+    let i = this.getSelectedPrimitiveIndex(primitives);
+    // If the first primitive is selected, unselect, otherwise select the previous.
+    this.setState({selectedPrimitive: (i == null || i == 0) ? null : primitives[i - 1]});
   }
 
   selectPrimitive(selectedPrimitive) {
@@ -48,24 +60,53 @@ export default class Toolbar extends Component {
     this.setState({selectedPrimitive});
   }
 
-  render() {
-    let primitives = [];
-    if (this.props.primitives) {
-      primitives = this.props.primitives.filter(this.state.search).primitives;
+  handleKeyDown = event => {
+    event.stopPropagation();
+    switch(SHARED.keyName(event)) {
+    case 'Esc':
+      event.target.blur();
+      return;
+    case 'Down':
+      this.next();
+      return;
+    case 'Up':
+      this.prev();
+      return;
     }
+  }
+
+  getPrimitives() {
+    if (this.props.primitives) {
+      return this.props.primitives.filter(this.state.search).primitives;
+    } else {
+      return [];
+    }
+  }
+
+  getSelectedPrimitiveIndex(primitives) {
+    for (let i = 0; i < primitives.length; i++) {
+      if (primitives[i] === this.state.selectedPrimitive) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  render() {
+    let primitives = this.getPrimitives();
     const selected = this.state.selectedPrimitive;
     return (
       <div className={classNames('blocks-ui Toolbar', {'has-selected':!!selected})}>
         <div className="search-box">
           <label className="screenreader-only" htmlFor="search_box">
             <h2>Search Functions</h2>
-          </label>            
+          </label>
           <input type="search"
             id="search_box"
             placeholder="Search functions"
             className="form-control"
             value={this.state.search}
-            onKeyDown={this.checkEscape}
+            onKeyDown={this.handleKeyDown}
             onChange={this.changeSearch} />
           {this.state.search ?
             <span className="glyphicon glyphicon-remove" onClick={this.clearSearch} />
