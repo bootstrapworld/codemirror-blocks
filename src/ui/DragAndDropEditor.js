8f1fdf94276ca51553e65b324d84d71d646ad6d0
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-04-23 08:37:52 -0400

    for DND calculation, ignore root nodes that CM has kept (hidden) onscreen for DOM measurement purposes (fixes #237)

--- src/ui/DragAndDropEditor.js
+++ src/ui/DragAndDropEditor.js
@@ -12,12 +12,19 @@ export default
 
 @DropNodeTarget(function(monitor) {
   let roots = SHARED.cm.getAllMarks().filter(m => m.BLOCK_NODE_ID);
+  let measureDOM = SHARED.cm.getWrapperElement().getElementsByClassName('CodeMirror-measure')[0];
   const {x: left, y: top} = monitor.getClientOffset();
-  let validTopLevelDrop = !roots.some(root => {
+  let notDroppedOnRootNode = !roots.some(root => {
+    // CM will keep the longest line in the DOM for scrollbar purposes, but use visibility:hidden
+    // if that line is outside of the viewport so that it doesn't show up. We need to check for
+    // this, since getBoundingClientRect() ignores whether an elt is hidden. 
+    if(window.getComputedStyle(root.replacedWith).visibility == "hidden") return false;
+    // for visible nodes, see if the drop location falls within the root's ClientRect
     let r = root.replacedWith.firstChild.getBoundingClientRect();
     return (r.left<left) && (left<r.right) && (r.top<top) && (top<r.bottom);
   });
-  if (validTopLevelDrop) {
+
+  if (notDroppedOnRootNode) {
     let loc = SHARED.cm.coordsChar({left, top});
     let dest = {from: loc, to: loc, isDropTarget: true};
     return this.props.dispatch(dropNode(monitor.getItem(), dest));
