df01b74defc6a110e8881b0f79fb72e68896a1fa
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2019-05-17 14:40:18 -0400

    remove cmb from stable pretty
    just parse and then use toString

--- spec/languages/pyret/StablePretty-test.js
+++ spec/languages/pyret/StablePretty-test.js
@@ -1,77 +1,29 @@
-import CodeMirrorBlocks from '../../../src/CodeMirrorBlocks';
-import pyret from '../../../src/languages/pyret';
-import {store} from '../../../src/store';
-import 'codemirror/addon/search/searchcursor.js';
-
-import {
-  _click,
-  _doubleClick,
-  _blur,
-  keyDown,
-  insertText,
-} from '../../support/simulate';
-
-import {_wait, cleanupAfterTest} from '../../support/test-utils';
-
-// ms delay to let the DOM catch up before testing
-const DELAY = 500;
+import PyretParser from 'codemirror-blocks/languages/pyret/PyretParser';
 
 describe('The CodeMirrorBlocks Class', function() {
   beforeEach(function() {
-    const fixture = `
-      <div id="root">
-        <div id="cmb-editor" class="editor-container"/>
-      </div>
-    `;
-    document.body.insertAdjacentHTML('afterbegin', fixture);
-    const container = document.getElementById('cmb-editor');
-    this.blocks = new CodeMirrorBlocks(container, {value: ""}, pyret);
-    this.blocks.setBlockMode(true);
-  });
-
-  afterEach(function() {
-    cleanupAfterTest('root', store);
+    this.parser = new PyretParser();
   });
 
   describe('testing method', function() {
     it("pretty-ify non-pretty text", function() {
-      this.blocks.setBlockMode(false);
-      this.blocks.setCursor({ line: 0, ch: 0 });
       let insert = "fun f(x):\n  x + 3\nend";
-      keyDown("9", {}, this.blocks.getInputField());
-      insertText(insert);
-      expect(this.blocks.getBlockMode()).toBe(false);
-      expect(this.blocks.getValue()).toBe(insert);
-      this.blocks.setBlockMode(true);
+      
       // see if pretty-printed now
-      expect(this.blocks.getValue()).not.toBe(insert);
-      expect(this.blocks.getValue()).toBe("fun f(x): x + 3 end");
+      expect(this.parser.parse(insert).toString()).not.toBe(insert);
+      expect(this.parser.parse(insert).toString()).toBe("fun f(x): x + 3 end");
     });
   });
 
   describe('small DS programs', function() {
-    beforeEach(async function() {
-      this.blocks.setBlockMode(true);
-      this.blocks.setValue("");
-      await DELAY;
-    });
-
     let testify = function (text, name = text, already_pretty = true) {
       return it(name, async function() {
-        this.blocks.setCursor({ line: 0, ch: 0 });
-        keyDown("9", {}, this.blocks.getInputField());
-        insertText(text);
-        await DELAY;
-
-        this.blocks.setBlockMode(false);
-        await DELAY;
-        // sometimes this is true???
-        expect(this.blocks.getBlockMode()).toBe(false);
+        let result = this.parser.parse(text).toString();
         if (already_pretty) {
-          expect(this.blocks.getValue()).toEqual(text);
+          expect(result).toEqual(text);
         }
         else {
-          expect(this.blocks.getValue()).not.toEqual(text);
+          expect(result).not.toEqual(text);
         }
       });
     };
@@ -140,18 +92,9 @@ end`);
   });
 
   describe("other pyret programs", function() {
-    beforeEach(async function () {
-      this.blocks.setBlockMode(true);
-      this.blocks.setValue("");
-      await DELAY;
-    });
-
     let testify = function(name, text) {
       it(name, async function () {
-        this.blocks.setValue(text);
-        this.blocks.setBlockMode(false);
-        await DELAY;
-        expect(this.blocks.getValue()).toEqual(text);
+        expect(this.parser.parse(text).toString()).toEqual(text);
       })
     };
 
