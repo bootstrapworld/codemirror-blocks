98dbcbcdf7e1c11b400a8ed0c769c867e8c3b306
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-08-09 21:37:36 -0400

    try turning off optimization when in travis

--- webpack/example.config.js
+++ webpack/example.config.js
@@ -44,7 +44,7 @@ module.exports = function(env, argv) {
       }),
       new webpack.IgnorePlugin(/analyzer|compiler|modules\.js/, /node_modules/)
     ]),
-    optimization: {
+    optimization: !('TRAVIS' in process.env && 'CI' in process.env) && {
       minimize: argv['mode'] == 'production',
       splitChunks: {
         cacheGroups: {
