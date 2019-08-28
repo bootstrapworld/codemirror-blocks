124694c7960e73de2c86d543edaf68b1fd0abca8
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-08-28 15:52:56 +0700

    upgrade react-dnd, along with other things

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,13 +1,14 @@
 import React  from 'react';
-import {DragDropContext} from 'react-dnd';
+import {DndProvider} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import {Provider} from 'react-redux';
 import {store} from '../store';
 
 export default WrappedComponent =>
-  DragDropContext(HTML5Backend)(
-    props =>
-    <Provider store={store}>
-      <WrappedComponent {...props} />
-    </Provider>
+  props => (
+    <DndProvider backend={HTML5Backend}>
+      <Provider store={store}>
+        <WrappedComponent {...props} />
+      </Provider>
+    </DndProvider>
   );
