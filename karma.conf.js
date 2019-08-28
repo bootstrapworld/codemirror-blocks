32ebdf14e6f5c07a6f88d1d08da736705c5c6ad4
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-08-28 15:09:09 -0400

    bump v. number and update dependencies. All tests pass locally

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,8 @@ module.exports = function(config) {
       // customShardStrategy: function(config) {
       //   config.executors // number, the executors set above
       //   config.shardIndex // number, the specific index for the shard currently running
-      //   config.description // string, the name of the top-level describe string. Useful //     for determining how to shard the current specs
+      //   config.description // string, the name of the top-level describe string. Useful 
+      //     for determining how to shard the current specs
       //   return config.
       // }
     },
