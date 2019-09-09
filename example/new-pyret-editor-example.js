ccc7787d3a42fff13ff07af47a5d6042fb715f80
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2019-09-09 12:57:10 -0400

    try making test infrastructure available in example page, for live-ish testing

--- example/new-pyret-editor-example.js
+++ example/new-pyret-editor-example.js
@@ -4,11 +4,23 @@ import './example-page.less';
 import dsExampleCode from './bootstrap-ds.arr';
 //import bigExampleCode from './ast-test.rkt';
 
+/// DEBUGGING STUFF
+import { wait, teardown } from '../spec/support/test-utils';
+import {
+  click,
+  keyDown,
+  _keyPress,
+  _insertText,
+} from '../spec/support/simulate';
+
+const DELAY = 250;
+
+
 
 //const smallExampleCode = `(+ 1 2) ;comment\n(+ 3 4)`;
-const smallExampleCode = `1 + 2`;
+const smallExampleCode = `load-spreadsheet("14er5Mh443Lb5SIFxXZHdAnLCuQZaA8O6qtgGlibQuEg")`;
 
-const useBigCode = true;
+const useBigCode = false;
 //const exampleCode = useBigCode ? bigExampleCode : smallExampleCode;
 const exampleCode = useBigCode? dsExampleCode : smallExampleCode;
 
@@ -20,3 +32,17 @@ editor.setBlockMode(true);
 // for debugging purposes
 window.editor = editor
 console.log(editor);
+
+document.getElementById('testButton').onclick = runTestEvent;
+
+async function runTestEvent(){
+	let ast = editor.getAst();
+    this.literal1 = ast.rootNodes[0];
+	console.log('before anything, activeElement is', document.activeElement);
+	click(this.literal1);
+    await wait(DELAY);
+    console.log('after clicking, activeElement is', document.activeElement)
+    keyDown("ArrowDown");
+    await wait(DELAY);
+    console.log('after ArrowDown, activeElement is', document.activeElement)
+}
