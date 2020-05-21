9688905e0ae7fb4a024e3b448958dadd054cfe04
Dorai Sitaram <ds26gte@gmail.com>
2020-05-21 16:00:51 -0400

    reduceers.js: Use log4js

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,8 @@
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+logger.level = 'ALL';
+
 const initialState = {
   selections: [],
   editable: {},
@@ -23,90 +28,90 @@ export const reducer = (
   switch (action.type) {
   case 'SET_FOCUS':
     result = {...state, focusId: action.focusId};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_AST':
     result = {...state, ast: action.ast, collapsedList: state.collapsedList.filter(action.ast.getNodeById)};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_SELECTIONS':
     result = {...state, selections: action.selections};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_EDITABLE':
     result = {...state, editable: {...state.editable, [action.id]: action.bool}};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_ERROR_ID':
     result = {...state, errorId: action.errorId};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'COLLAPSE':
     result = {...state, collapsedList: state.collapsedList.concat([action.id])};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'UNCOLLAPSE':
     result = {...state, collapsedList: state.collapsedList.filter(e => e !== action.id)};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'COLLAPSE_ALL':
     result = {...state, collapsedList: [...state.ast.nodeIdMap.keys()]};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'UNCOLLAPSE_ALL':
     result = {...state, collapsedList: []};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_CURSOR':
     result = {...state, cur: action.cur};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'DISABLE_QUARANTINE':
     result = {...state, quarantine: null};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'CHANGE_QUARANTINE':
     result = {...state, quarantine: [state.quarantine[0], state.quarantine[1], action.text]};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_QUARANTINE':
     result = {...state, quarantine: [action.start, action.end, action.text]};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'SET_ANNOUNCER':
     result = {...state, announcer: action.announcer};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'ADD_MARK':
     result = {...state, markedMap: state.markedMap.set(action.id, action.mark)};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'CLEAR_MARK':
     state.markedMap.delete(action.id);
     result = {...state};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'DO':
     result = {...state, undoFocusStack: [...state.undoFocusStack, action.focus], redoFocusStack: []};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'UNDO':
     state.redoFocusStack.push(state.undoFocusStack.pop());
     result = {...state};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'REDO':
     state.undoFocusStack.push(state.redoFocusStack.pop());
     result = {...state};
-    activities.push(result);
+    logger.debug(result);
     return result;
   case 'RESET_STORE_FOR_TESTING':
     result =  initialState;
-    activities.push(result);
+    logger.debug(result);
     return result;
   default:
     result =  state;
-    activities.push(result);
+    logger.debug(result);
     return result;
   }
 };
