76022a76565a80c8ceeae202e0ded1698828309c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-06-14 15:45:45 -0400

    Upgrade React-DND to v11

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,6 +1,6 @@
 import React  from 'react';
 import {DndProvider} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 import {Provider} from 'react-redux';
 import {store} from '../store';
 
