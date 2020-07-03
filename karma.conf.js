85f47255f4f85dbbfee9f99113a345140ac9f139
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-07-02 23:56:37 -0400

    load coverage before coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,11 @@
 // Generated on Mon Nov 30 2015 13:06:12 GMT-0800 (PST)
 var webpackConfig = require('./webpack/test.config.js');
 var envConfig = require('./env-config.js');
-var reporters = ['jasmine-diff', 'dots'];
+var reporters = ['jasmine-diff', 'dots', 'coverage'];
 
 
 if (envConfig.runCoverage) {
-  reporters.push('coverage');
+  //reporters.push('coverage');
 
   if (envConfig.isCI) {
     reporters.push('coveralls');
