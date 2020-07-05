5197cb87ca1faa173b1905a747cd7059b11798b9
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-07-05 09:10:05 -0400

    we were losing the ASThint returned from speculateChanges, resulting in an extra (time-consuming) parse and AST construction in commitChanges

--- src/edits/speculateChanges.js
+++ src/edits/speculateChanges.js
@@ -18,8 +18,8 @@ export function speculateChanges(changeArr) {
   }
   let newText = tmpCM.getValue();
   try {
-    let newAST = SHARED.parser.parse(newText);
-    return {successful: true, newAST};
+    let newAST = SHARED.parser.parse(newText, false);
+    return {successful: true, newAST: newAST};
   } catch (exception) {
     return {successful: false, exception};
   }
