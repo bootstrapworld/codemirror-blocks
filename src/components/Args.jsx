c24e122e3332b5855d0fa089cfb5098cef1bd3a9
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-09-23 13:48:57 -0400

    remove some unecessary classes in favor of functions

--- src/components/Args.jsx
+++ src/components/Args.jsx
@@ -1,24 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import {ASTNode} from '../ast';
 import {DropTarget} from './DropTarget';
-import {span} from '../types';
 
-export default class Args extends Component {
-  static propTypes = {
-    field: PropTypes.string.isRequired,
-    children: PropTypes.arrayOf(PropTypes.instanceOf(ASTNode)).isRequired,
-  }
+const Args = props => {
+  let {children} = props;
+  const elems = [];
+  elems.push(<DropTarget key={'drop-0'} field={props.field}/>);
+  children.forEach((child, index) => {
+    elems.push(child.reactElement({key : 'node' + index}));
+    elems.push(<DropTarget key={'drop-' + (index+1)} field={props.field}/>);
+  });
+  return elems;
+};
 
-  render() {
-    let {children} = this.props;
-    const elems = [];
-    elems.push(<DropTarget key={'drop-0'} field={this.props.field}/>);
-    children.forEach((child, index) => {
-      elems.push(child.reactElement({key : 'node' + index}));
-      elems.push(<DropTarget key={'drop-' + (index+1)} field={this.props.field}/>);
-    });
-    return elems;
-  }
-}
+Args.propTypes = {
+  field: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.instanceOf(ASTNode)).isRequired,
+};
+
+export default Args
