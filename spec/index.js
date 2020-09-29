2dfa74c5daebcc7f77eeb1fe8828bc382e86500e
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-09-29 08:15:08 -0400

    turn on all tests

--- spec/index.js
+++ spec/index.js
@@ -1,3 +1,3 @@
 // require all the files in the spec folder that end with -test.js
-var context = require.context('.', true, /.redo-test\.js$/);
+var context = require.context('.', true, /.-test\.js$/);
 context.keys().forEach(context);
