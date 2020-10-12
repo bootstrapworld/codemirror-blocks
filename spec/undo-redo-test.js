85aeae8c65d0324138b9167b87edf2911fdf656a
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-10-12 13:51:03 -0400

    try replaceRange one more time

--- spec/undo-redo-test.js
+++ spec/undo-redo-test.js
@@ -44,7 +44,7 @@ describe("when testing undo/redo,", function () {
     await wait(DELAY);
     expect(this.cmb.getValue()).toEqual('\nB\n\n');
     expect(this.cmb.historySize()).toEqual({undo: 2, redo: 0});
-    insertText("C");                                        // change (3): insert C at the end
+    this.cmb.replaceRange("C", {line:3,ch:0},{line:3,ch:0});// change (3): insert C at the end
     await wait(DELAY);
     expect(this.cmb.getValue()).toEqual('\nB\n\nC');
     expect(this.cmb.historySize()).toEqual({undo: 3, redo: 0});
