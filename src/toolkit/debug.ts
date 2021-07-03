a58611cbe3c829431f37002d4f479697fde5e32e
Paul Carduner <paul@carduner.net>
2021-07-02 17:16:34 -0700

    Fill out the rest of createDebuggingInterface function

--- src/toolkit/debug.ts
+++ src/toolkit/debug.ts
@@ -1,7 +1,15 @@
-import "../../example/example-page.less";
-import type { Language } from "../CodeMirrorBlocks";
-import CodeMirrorBlocks from "../CodeMirrorBlocks";
+import '../../example/example-page.less';
+import type { Language } from '../CodeMirrorBlocks';
+import CodeMirrorBlocks from '../CodeMirrorBlocks';
 
+/**
+ * Renders the codemirror blocks editor to the page along with some
+ * debugging tools for downloading and replaying reducer action logs
+ * 
+ * @param language the language definition to use when instantiating the editor
+ * @param value Initial string of code to load the codemirror blocks editor with
+ * @returns the codemirror blocks editor
+ */
 export function createDebuggingInterface(language: Language, value: string) {
   document.body.innerHTML = `
   <div class="container editor-example">
@@ -25,8 +33,94 @@ export function createDebuggingInterface(language: Language, value: string) {
   `;
 
   const container = document.getElementById('cmb-editor');
-  return CodeMirrorBlocks(container, {
-    collapseAll: false, 
-    value,
-  }, language);
+  // grab the load-from-log button
+  const loadLogButton = document.getElementById(
+    'loadFromLog'
+  ) as HTMLInputElement;
+  const downloadLogButton = document.getElementById(
+    'downloadLog'
+  ) as HTMLButtonElement;
+  const nextButton = document.getElementById('nextButton') as HTMLButtonElement;
+
+  const editor = CodeMirrorBlocks(
+    container,
+    {
+      collapseAll: false,
+      value,
+    },
+    language
+  );
+
+  let history,
+    lastAction,
+    currentAction = 0;
+
+  downloadLogButton.onclick = () => {
+    let json = { history: window.reducerActivities, exception: 'DUMMY' };
+
+    var element = document.createElement('a');
+    element.setAttribute(
+      'href',
+      'data:text/plain;charset=utf-8,' +
+        encodeURIComponent(JSON.stringify(json))
+    );
+    element.setAttribute('download', 'CMB Log.txt');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  // When a file is loaded, read it
+  loadLogButton.onchange = (e) => {
+    let file = (e.target as HTMLInputElement).files[0];
+    let reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+    // parse the string, draw the actions, and set up counters
+    // and UI for replaying them
+    reader.onload = (readEvent) => {
+      let log;
+      try {
+        log = JSON.parse(readEvent.target.result.toString());
+        if (!(log.exception && log.history)) throw 'Bad Log';
+      } catch {
+        console.log(readEvent.target.result.toString());
+        alert('Malformed log file!\nContents printed to console');
+        return;
+      }
+      editor.setValue('');
+      editor.setBlockMode(true);
+      editor.resetNodeCounter();
+      history = log.history;
+      history.forEach((entry) => {
+        let LI = document.createElement('LI');
+        LI.className = 'logEntry';
+        LI.innerHTML = JSON.stringify(entry);
+        document.getElementById('entries').appendChild(LI);
+      });
+      lastAction = history.length;
+      nextButton.style.display = 'inline-block';
+      loadLogButton.style.display = 'none';
+    };
+  };
+
+  // Highlight the active entry and pass it to the editor
+  // Once we've gone through all of them, change the UI
+  nextButton.onclick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let entries = [...document.getElementById('entries').children] as HTMLElement[];
+    entries.forEach((c) => (c.style.background = 'none'));
+    entries[currentAction].style.background = 'lightblue';
+    editor.executeAction(history[currentAction]);
+    currentAction++;
+    if (currentAction == lastAction) {
+      nextButton.value = 'Done';
+      nextButton.disabled = true;
+    }
+    editor.focus();
+  };
+
+  return editor;
 }
