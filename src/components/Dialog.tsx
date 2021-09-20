e90cee11ca724cd23b37722ce084b9cc8d01b367
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2021-09-20 17:27:51 -0400

    move dialog component over to hooks

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,4 +1,10 @@
-import React, { Component, KeyboardEvent, ReactElement } from "react";
+import React, { 
+  Component, 
+  KeyboardEvent, 
+  ReactElement, 
+  useEffect,
+  useRef,
+} from "react";
 import Modal from "react-modal";
 import PropTypes from "prop-types";
 import "../less/Dialog.less";
@@ -14,57 +20,47 @@ type Props = {
   keyUp?: React.KeyboardEventHandler<HTMLDivElement>;
 };
 
-export default class Dialog extends Component<Props> {
-  titleRef: React.Ref<unknown>;
-  constructor(props: Props) {
-    super(props);
-    this.titleRef = React.createRef();
-  }
+const Dialog = (props: Props) => {
+  const { appElement, isOpen, closeFn, keyUp, body } = props;
+  var headerRef = useRef<HTMLHeadingElement>(null);
 
-  static propTypes = {
-    appElement: PropTypes.object.isRequired,
-    isOpen: PropTypes.bool.isRequired,
-    closeFn: PropTypes.func.isRequired,
-    keyUp: PropTypes.func,
-    body: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]).isRequired,
+  useEffect(() => {
+   Modal.setAppElement(appElement);
+  }, []);
+
+  const onKeyUp = (e: KeyboardEvent): void => {
+    if (e.key === "Escape") closeFn();
   };
 
-  onKeyUp = (e: KeyboardEvent) => {
-    if (e.key === "Escape") this.props.closeFn();
-  };
-
-  componentDidMount() {
-    Modal.setAppElement(this.props.appElement);
+  // NOTE(Emmanuel): this feels like a level of indirection
+  // that refs are supposed to deal with. 
+  const focusTitle = () => {
+    headerRef.current.focus();
   }
 
-  focusTitle() {
-    document.getElementById("heading").focus();
-  }
-
-  render() {
-    const { appElement, isOpen, closeFn, keyUp, body } = this.props;
-    return (
-      <Modal
-        isOpen={isOpen}
-        className={"madeUpClassToCancelDefaultStyles"}
-        onRequestClose={closeFn}
-        shouldCloseOnEsc={true}
-        shouldReturnFocusAfterClose={true}
-        shouldFocusAfterRender={true}
-        shouldCloseOnOverlayClick={true}
-        onAfterOpen={this.focusTitle}
-        aria={{ labelledby: "heading" }}
-      >
-        <div tabIndex={-1} id="DialogContents" onKeyUp={keyUp || this.onKeyUp}>
-          <h1 tabIndex={-1} id="heading">
-            {this.props.body?.title}
-          </h1>
-          {this.props.body?.content}
-          <button className="closeDialog" onClick={() => this.props.closeFn()}>
-            Close
-          </button>
-        </div>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      className={"madeUpClassToCancelDefaultStyles"}
+      onRequestClose={closeFn}
+      shouldCloseOnEsc={true}
+      shouldReturnFocusAfterClose={true}
+      shouldFocusAfterRender={true}
+      shouldCloseOnOverlayClick={true}
+      onAfterOpen={focusTitle}
+      aria={{ labelledby: "heading" }}
+    >
+      <div tabIndex={-1} id="DialogContents" onKeyUp={keyUp || onKeyUp}>
+        <h1 tabIndex={-1} ref={headerRef}>
+          {props.body?.title}
+        </h1>
+        {props.body?.content}
+        <button className="closeDialog" onClick={() => closeFn()}>
+          Close
+        </button>
+      </div>
+    </Modal>
+  );
 }
+
+export default Dialog;
