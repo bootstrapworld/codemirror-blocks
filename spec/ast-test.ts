d2c7e5e16e594d3b555717882ae9261fc8c0f12d
Paul Carduner <paul@carduner.net>
2021-10-03 10:43:20 -0700

    convert ast-test to typescript

--- spec/ast-test.ts
+++ spec/ast-test.ts
@@ -1,15 +1,11 @@
-import { AST } from "codemirror-blocks/ast";
-import {
-  Literal,
-  Sequence,
-  FunctionApp,
-  Comment,
-} from "codemirror-blocks/nodes";
+import CodeMirror from "codemirror";
+import { AST } from "../src/CodeMirrorBlocks";
+import { FunctionApp, Literal, Sequence, Comment } from "../src/nodes";
 
 console.log("Doing ast-test.js");
 
-describe("The Literal Class", function () {
-  it("should be constructed with a value and data type", function () {
+describe("The Literal Class", () => {
+  it("should be constructed with a value and data type", () => {
     const from = { line: 0, ch: 0 };
     const to = { line: 0, ch: 2 };
     const literal = new Literal(from, to, 11, "number");
@@ -20,17 +16,17 @@ describe("The Literal Class", function () {
     expect(literal.options).toEqual({});
   });
 
-  it("should set a default data type of unknown if one isn't provided", function () {
+  it("should set a default data type of unknown if one isn't provided", () => {
     const literal = new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11);
     expect(literal.dataType).toBe("unknown");
   });
 
-  it("should only return itself when iterated over", function () {
+  it("should only return itself when iterated over", () => {
     const literal = new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11);
     expect([...literal.descendants()]).toEqual([literal]);
   });
 
-  it("should take an optional options parameter in its constructor", function () {
+  it("should take an optional options parameter in its constructor", () => {
     const literal = new Literal(
       { line: 0, ch: 0 },
       { line: 0, ch: 2 },
@@ -42,10 +38,16 @@ describe("The Literal Class", function () {
   });
 });
 
-describe("The Sequence Class", function () {
-  let sequence, expression1, expression2, from, to, name, exprs;
+describe("The Sequence Class", () => {
+  let sequence: Sequence;
+  let expression1: FunctionApp;
+  let expression2: FunctionApp;
+  let from: CodeMirror.Position;
+  let to: CodeMirror.Position;
+  let name: Literal;
+  let exprs: FunctionApp[];
 
-  beforeEach(function () {
+  beforeEach(() => {
     // (+ 1 2)
     const func1 = new Literal(
       { line: 0, ch: 8 },
@@ -87,28 +89,37 @@ describe("The Sequence Class", function () {
     // (begin (+ 1 2) (- 2 3))
     from = { line: 0, ch: 0 };
     to = { line: 0, ch: 23 };
-    name = "begin";
+    name = new Literal(
+      { line: 0, ch: 1 },
+      { line: 0, ch: 6 },
+      "begin",
+      "symbol"
+    );
     exprs = [expression1, expression2];
     sequence = new Sequence(from, to, exprs, name);
   });
 
-  it("should be constructed with a list of expressions", function () {
+  it("should be constructed with a list of expressions", () => {
     expect(sequence.from).toBe(from);
     expect(sequence.to).toBe(to);
     expect(sequence.exprs).toBe(exprs);
     expect(sequence.name).toEqual(name);
   });
 
-  it("should take an optional options parameter in its constructor", function () {
+  it("should take an optional options parameter in its constructor", () => {
     const options = { "aria-label": "sequence" };
     const newSequence = new Sequence(from, to, exprs, name, options);
     expect(newSequence.options).toEqual(options);
   });
 });
 
-describe("The FunctionApp Class", function () {
-  let expression, func, args, nestedExpression, ast;
-  beforeEach(function () {
+describe("The FunctionApp Class", () => {
+  let expression: FunctionApp;
+  let func: Literal;
+  let args: Literal[];
+  let nestedExpression: FunctionApp;
+  let ast: AST.AST;
+  beforeEach(() => {
     func = new Literal({ line: 1, ch: 1 }, { line: 1, ch: 2 }, "+", "symbol");
     args = [
       new Literal({ line: 1, ch: 3 }, { line: 1, ch: 5 }, 11),
@@ -141,16 +152,16 @@ describe("The FunctionApp Class", function () {
       ]
     );
     // build the AST, thereby assigning parent/child/sibling relationships
-    ast = new AST([expression]);
+    ast = new AST.AST([expression]);
   });
 
-  it("should take a function name and list of args in its constructor", function () {
+  it("should take a function name and list of args in its constructor", () => {
     expect(expression.args).toBe(args);
     expect(expression.func).toBe(func);
     expect(expression.options).toEqual({ "aria-label": "+ expression" });
   });
 
-  it("should return itself and its descendants when iterated over", function () {
+  it("should return itself and its descendants when iterated over", () => {
     expect([...nestedExpression.descendants()]).toEqual([
       nestedExpression,
       nestedExpression.func,
@@ -159,7 +170,7 @@ describe("The FunctionApp Class", function () {
     ]);
   });
 
-  it("should have all navigation pointers and aria attributes set", function () {
+  it("should have all navigation pointers and aria attributes set", () => {
     expect(expression.next).toEqual(expression.func);
     expect(ast.getNodeParent(expression.func)).toEqual(expression);
     expect(ast.getNodeAfter(expression.func)).toEqual(expression.args[0]);
@@ -172,14 +183,14 @@ describe("The FunctionApp Class", function () {
   });
 });
 
-describe("The AST Class", function () {
-  it("should take a set of root nodes in its constructor", function () {
+describe("The AST Class", () => {
+  it("should take a set of root nodes in its constructor", () => {
     const nodes = [new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11)];
-    const ast = new AST(nodes);
+    const ast = new AST.AST(nodes);
     expect(ast.rootNodes).toBe(nodes);
   });
 
-  it("should add every node to a node map for quick lookup", function () {
+  it("should add every node to a node map for quick lookup", () => {
     const nodes = [
       new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11),
       new FunctionApp(
@@ -191,15 +202,15 @@ describe("The AST Class", function () {
           new Literal({ line: 1, ch: 6 }, { line: 0, ch: 8 }, 22),
         ]
       ),
-    ];
-    const ast = new AST(nodes);
+    ] as [Literal, FunctionApp];
+    const ast = new AST.AST(nodes);
     expect(ast.nodeIdMap.get(nodes[0].id)).toBe(nodes[0]);
     expect(ast.nodeIdMap.get(nodes[1].id)).toBe(nodes[1]);
     expect(ast.nodeIdMap.get(nodes[1].args[0].id)).toBe(nodes[1].args[0]);
     expect(ast.nodeIdMap.get(nodes[1].args[1].id)).toBe(nodes[1].args[1]);
   });
 
-  it("idential subtrees should have the same hash", function () {
+  it("idential subtrees should have the same hash", () => {
     const nodes1 = [
       new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11),
       new FunctionApp(
@@ -224,12 +235,12 @@ describe("The AST Class", function () {
         ]
       ),
     ];
-    const ast1 = new AST(nodes1);
-    const ast2 = new AST(nodes2);
+    const ast1 = new AST.AST(nodes1);
+    const ast2 = new AST.AST(nodes2);
     expect(ast1.rootNodes[0].hash).toBe(ast2.rootNodes[0].hash);
   });
 
-  it("idential subtrees with different comments should have different hashes", function () {
+  it("idential subtrees with different comments should have different hashes", () => {
     const nodes1 = [
       new Literal({ line: 0, ch: 0 }, { line: 0, ch: 2 }, 11, "Number", {
         comment: new Comment({ line: 0, ch: 4 }, { line: 0, ch: 7 }, "moo"),
@@ -256,8 +267,8 @@ describe("The AST Class", function () {
         ]
       ),
     ];
-    const ast1 = new AST(nodes1);
-    const ast2 = new AST(nodes2);
+    const ast1 = new AST.AST(nodes1);
+    const ast2 = new AST.AST(nodes2);
     expect(ast1.rootNodes[0].hash).not.toBe(ast2.rootNodes[0].hash);
   });
 });
