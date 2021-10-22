89dbe85dd2e929c9513fb8b0a6d5ee85d9c16e09
pcardune <paul@carduner.net>
2021-10-22 11:44:05 -0700

    first version of wescheme.org bundle config

--- packages/wescheme-blocks/webpack.config.js
+++ packages/wescheme-blocks/webpack.config.js
@@ -1,59 +1,57 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const {
   getWebpackDevServerConfig,
-  getWebpackBundleConfig,
+  getBaseConfig,
 } = require("codemirror-blocks/lib/toolkit/webpack");
 
 const devServerConfig = getWebpackDevServerConfig({
   context: path.resolve(__dirname, "dev-server"),
   entry: "./index.js",
 });
-const bundleConfig = getWebpackBundleConfig({
-  entry: {
-    CodeMirrorBlocks: path.resolve(
-      __dirname,
-      "src",
-      "languages",
-      "wescheme",
-      "index"
-    ),
-  },
-});
-
-const alias = {
-  jsnums: "wescheme-js/src/runtime/js-numbers",
-  lex: "wescheme-js/src/lex",
-  types: "wescheme-js/src/runtime/types",
-  structs: "wescheme-js/src/structures",
-};
-
 devServerConfig.module.rules.push({
   test: /\.rkt$/,
   use: [{ loader: "raw-loader" }],
 });
 
-// Add aliases needed by WeschemeParser.js
-// TODO(pcardune): stop using aliases and just import
-// directly from the right place...?
-devServerConfig.resolve.alias = alias;
-bundleConfig.resolve.alias = alias;
+const baseConfig = getBaseConfig();
 
-bundleConfig.externals = {
-  codemirror: "codemirror",
-  "codemirror/addon/search/search": "codemirror",
-  "codemirror/addon/search/searchcursor": "codemirror",
-  "wescheme-js/src/runtime/js-numbers": "jsnums",
-  "wescheme-js/src/lex": {
-    commonjs: "plt.compiler",
-    commonjs2: "plt.compiler",
-    root: ["plt", "compiler"],
+const bundleConfig = {
+  ...baseConfig,
+  name: "bundle",
+  mode: "production",
+  entry: {
+    CodeMirrorBlocks: path.resolve(__dirname, "src", "wescheme.org"),
   },
-  "wescheme-js/src/runtime/types": "types",
-  "wescheme-js/src/structures": {
-    commonjs: "plt.compiler",
-    commonjs2: "plt.compiler",
-    root: ["plt", "compiler"],
+  output: {
+    filename: "WeschemeBlocks.js",
+    clean: true,
+    library: {
+      name: "WeschemeBlocks",
+      type: "window",
+      export: "default",
+    },
   },
 };
+
+// TODO(pcardune): figure out what needs to be part of the bundle
+// for wescheme.org
+// bundleConfig.externals = {
+//   codemirror: "codemirror",
+//   "codemirror/addon/search/search": "codemirror",
+//   "codemirror/addon/search/searchcursor": "codemirror",
+//   "wescheme-js/src/runtime/js-numbers": "jsnums",
+//   "wescheme-js/src/lex": {
+//     commonjs: "plt.compiler",
+//     commonjs2: "plt.compiler",
+//     root: ["plt", "compiler"],
+//   },
+//   "wescheme-js/src/runtime/types": "types",
+//   "wescheme-js/src/structures": {
+//     commonjs: "plt.compiler",
+//     commonjs2: "plt.compiler",
+//     root: ["plt", "compiler"],
+//   },
+// };
 
 module.exports = [devServerConfig, bundleConfig];
