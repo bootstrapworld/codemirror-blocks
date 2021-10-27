653a498a21e48753439f8b29586b80a8d13e6e9c
pcardune <paul@carduner.net>
2021-10-26 18:12:29 -0700

    Simplify patchAst a bit

--- packages/codemirror-blocks/src/edits/patchAst.ts
+++ packages/codemirror-blocks/src/edits/patchAst.ts
@@ -1,18 +1,4 @@
 import { ASTNode } from "../ast";
-import { assert } from "../utils";
-
-// defaultdict with empty list
-function addIndex<V>(
-  container: Record<string | number, V[]>,
-  k: string | number,
-  v: V
-) {
-  if (container[k]) {
-    container[k].push(v);
-  } else {
-    container[k] = [v];
-  }
-}
 
 function copyAllIds(oldTree: ASTNode, newTree: ASTNode) {
   const oldIter = oldTree.descendants()[Symbol.iterator]();
@@ -20,7 +6,11 @@ function copyAllIds(oldTree: ASTNode, newTree: ASTNode) {
   let oldPtr = oldIter.next();
   let newPtr = newIter.next();
   while (!oldPtr.done) {
-    assert(!newPtr.done);
+    if (newPtr.done) {
+      throw new Error(
+        "copyAllIds failed. Expected newTree to have same number of descendants as old tree"
+      );
+    }
     newPtr.value.id = oldPtr.value.id;
     newPtr.value.element = oldPtr.value.element;
     oldPtr = oldIter.next();
@@ -28,7 +18,7 @@ function copyAllIds(oldTree: ASTNode, newTree: ASTNode) {
   }
 }
 
-export default function unify(oldTree: ASTNode[], newTree: ASTNode[]) {
+export default function patchAst(oldTree: ASTNode[], newTree: ASTNode[]) {
   function children(node: ASTNode | ASTNode[]): ASTNode[] {
     if (node instanceof ASTNode) {
       return [...node.children()];
@@ -40,32 +30,37 @@ export default function unify(oldTree: ASTNode[], newTree: ASTNode[]) {
     if (newTree instanceof ASTNode && oldTree instanceof ASTNode) {
       newTree.id = oldTree.id;
     }
-    const index: { [key: string]: ASTNode[] } = {};
+
+    // build up a mapping of node hashes to the nodes matching that hash
+    const hashToNodeMap: { [key: number]: ASTNode[] } = {};
     for (const oldNode of children(oldTree)) {
-      addIndex(index, oldNode.hash, oldNode);
-    }
-    for (const key in index) {
-      index[key].reverse();
+      if (hashToNodeMap[oldNode.hash]) {
+        hashToNodeMap[oldNode.hash].push(oldNode);
+      } else {
+        hashToNodeMap[oldNode.hash] = [oldNode];
+      }
     }
 
     const processed = new Set();
 
     let partiallySuccess = false;
-    const newLeftover = children(newTree).filter((newNode) => {
-      if (index[newNode.hash]?.length > 0) {
-        const oldNode = index[newNode.hash].pop();
+    const newLeftover = [];
+    for (const newNode of children(newTree)) {
+      if (hashToNodeMap[newNode.hash]?.length > 0) {
+        const oldNode = hashToNodeMap[newNode.hash].shift();
         if (oldNode) {
           copyAllIds(oldNode, newNode);
           partiallySuccess = true;
           processed.add(oldNode.id);
-          return false;
         }
+      } else {
+        newLeftover.push(newNode);
       }
-      return true;
-    });
+    }
     const oldLeftover = children(oldTree).filter((oldNode) => {
       return !processed.has(oldNode.id);
     });
+
     if (partiallySuccess || newLeftover.length <= 1) {
       const commonLength = Math.min(oldLeftover.length, newLeftover.length);
       for (let i = 0; i < commonLength; i++) {
