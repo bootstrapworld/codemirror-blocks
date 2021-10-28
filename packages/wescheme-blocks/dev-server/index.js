ddeae72cf093437f0d2cb2e192d4cc9ed5852333
pcardune <paul@carduner.net>
2021-10-28 09:35:08 -0700

    Break out cmb-toolkit package

--- packages/wescheme-blocks/dev-server/index.js
+++ packages/wescheme-blocks/dev-server/index.js
@@ -1,19 +1,25 @@
-import { createDebuggingInterface } from "codemirror-blocks/lib/toolkit/debug";
-
+import "./index.less";
+import CodeMirrorBlocks from "codemirror-blocks";
 import { WeScheme } from "../src/languages/wescheme";
 import bigExampleCode from "./ast-test.rkt";
-
-// HACK: expose ALL test utilities, events, etc
-// so they can be used from the browser console
-import * as t from "codemirror-blocks/lib/toolkit/test-utils";
-Object.assign(window, t);
 
 const smallExampleCode = `(+ 1 2) ;comment\n(+ 3 4)`;
 
 const useBigCode = true;
 const exampleCode = useBigCode ? bigExampleCode : smallExampleCode;
 
-const editor = createDebuggingInterface(WeScheme, exampleCode);
+const container = document.createElement("div");
+document.body.appendChild(container);
+container.id = "app";
+
+const editor = CodeMirrorBlocks(
+  container,
+  {
+    collapseAll: false,
+    value: exampleCode,
+  },
+  WeScheme
+);
 
 // for debugging purposes
 window.editor = editor;
