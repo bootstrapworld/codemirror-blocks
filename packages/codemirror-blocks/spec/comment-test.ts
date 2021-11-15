5dbb9f7b04511d83f59457542add5e8a1ae6f673
pcardune <paul@carduner.net>
2021-11-15 15:00:21 -0800

    Reenable comment insertion tests

--- packages/codemirror-blocks/spec/comment-test.ts
+++ packages/codemirror-blocks/spec/comment-test.ts
@@ -1,16 +1,13 @@
 import { act } from "@testing-library/react";
-import { ASTNode } from "../src/ast";
 import { API } from "../src/CodeMirrorBlocks";
 import { Pos } from "../src/editor";
 import wescheme from "../src/languages/wescheme";
 import * as actions from "../src/state/actions";
 import { AppStore } from "../src/state/store";
 
-import { wait, teardown, click, mountCMB } from "../src/toolkit/test-utils";
+import { teardown, mountCMB, keyDown } from "../src/toolkit/test-utils";
 
-const QUARANTINE_DELAY = 2000;
-
-describe("When editing and moving commented nodes", function () {
+describe("When editing and moving commented nodes", () => {
   let cmb!: API;
   let store!: AppStore;
   const setQuarantine = (start: Pos, end: Pos, text: string) =>
@@ -23,25 +20,20 @@ describe("When editing and moving commented nodes", function () {
     store = mounted.store;
   });
 
-  afterEach(function () {
+  afterEach(() => {
     teardown();
   });
 
-  describe("inserting comments", function () {
-    let expr0!: ASTNode;
-
-    beforeEach(async function () {
+  describe("inserting comments", () => {
+    beforeEach(() => {
       cmb.setValue(`
 (comment free)
 1; comment1
 #| comment2 |#
 2`);
-      cmb.setBlockMode(true);
-      const ast = cmb.getAst();
-      expr0 = ast.rootNodes[0];
     });
 
-    it("when the mode is toggled, it should reformat all comments as block comments", async function () {
+    it("when the mode is toggled, it should reformat all comments as block comments", () => {
       cmb.setBlockMode(false);
       // the opening whitespace should be removed!
       expect(cmb.getValue()).toBe(`(comment free)
@@ -50,11 +42,15 @@ describe("When editing and moving commented nodes", function () {
 2`);
     });
 
-    // TODO(pcardune) reenable
-    xit("you should be able to insert a commented node after a commented node", async function () {
-      setQuarantine({ line: 3, ch: 1 }, { line: 3, ch: 1 }, "1 #| comment1 |#");
-      await wait(QUARANTINE_DELAY);
-      click(expr0);
+    it("you should be able to insert a commented node after a commented node", () => {
+      const commentedNode = cmb.getAst().rootNodes[2];
+      expect(commentedNode.toString()).toMatchInlineSnapshot(`
+        "#| comment2 |#
+        2"
+      `);
+      setQuarantine(commentedNode.to, commentedNode.to, "1 #| comment1 |#");
+      keyDown("Enter");
+      cmb.setBlockMode(false);
       expect(cmb.getValue()).toBe(`(comment free)
 1 #| comment1 |#
 #| comment2 |#
@@ -62,15 +58,14 @@ describe("When editing and moving commented nodes", function () {
 1 #| comment1 |#`);
     });
 
-    // TODO(pcardune) reenable
-    xit("you should be able to insert a commented node after an uncommented node", async function () {
-      setQuarantine(
-        { line: 0, ch: 14 },
-        { line: 0, ch: 14 },
-        "1 #| comment1 |#"
+    it("you should be able to insert a commented node after an uncommented node", () => {
+      const uncommentedNode = cmb.getAst().rootNodes[0];
+      expect(uncommentedNode.toString()).toMatchInlineSnapshot(
+        `"(comment free)"`
       );
-      await wait(QUARANTINE_DELAY);
-      click(expr0);
+      setQuarantine(uncommentedNode.to, uncommentedNode.to, "1 #| comment1 |#");
+      keyDown("Enter");
+      cmb.setBlockMode(false);
       expect(cmb.getValue()).toBe(`(comment free)
 1 #| comment1 |#
 1 #| comment1 |#
